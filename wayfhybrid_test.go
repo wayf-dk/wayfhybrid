a74a4864cb436e8861e1edec3c3ffed8620a48d4
Mehran Khan <mekhan@test-wayf-03.test.lan>
2018-04-23 12:43:16 +0000

    gofmt

--- wayfhybrid_test.go
+++ wayfhybrid_test.go
@@ -58,7 +58,7 @@ func ExampleNewMetadata() {
 	}
 	// Output:
 	// one true
-    // two true
+	// two true
 }
 
 func ExampleCheckCprCentury() {
@@ -272,59 +272,59 @@ func ExampleHandleAttributeNameFormat() {
 }
 
 func ExampleCheckScope() {
-    response := goxml.NewXpFromFile("testdata/sourceresponse_dtu.saml")
-    idp_md := goxml.NewXpFromFile("testdata/scope.xml")
-    destinationAttributes := response.Query(nil, `/samlp:Response/saml:Assertion/saml:AttributeStatement`)[0]
-    eppn, securitydomain, err := checkScope(response, idp_md, destinationAttributes, "saml:Attribute[@Name='eduPersonPrincipalName']/saml:AttributeValue")
-    fmt.Println(eppn, securitydomain, err)
-    // Output:
-    // madpe@dtu.dk dtu.dk <nil>
+	response := goxml.NewXpFromFile("testdata/sourceresponse_dtu.saml")
+	idp_md := goxml.NewXpFromFile("testdata/scope.xml")
+	destinationAttributes := response.Query(nil, `/samlp:Response/saml:Assertion/saml:AttributeStatement`)[0]
+	eppn, securitydomain, err := checkScope(response, idp_md, destinationAttributes, "saml:Attribute[@Name='eduPersonPrincipalName']/saml:AttributeValue")
+	fmt.Println(eppn, securitydomain, err)
+	// Output:
+	// madpe@dtu.dk dtu.dk <nil>
 }
 
 func ExampleCheckScopeAAU() {
-    response := goxml.NewXpFromFile("testdata/sourceresponse_dtu.saml")
-    idp_md := goxml.NewXpFromFile("testdata/scope.xml")
-    response.QueryDashP(nil, `/saml:Assertion/saml:AttributeStatement[1]/saml:Attribute[@Name='eduPersonPrincipalName']/saml:AttributeValue`, "mekhan@student.aau.dk@aau.dk", nil)
+	response := goxml.NewXpFromFile("testdata/sourceresponse_dtu.saml")
+	idp_md := goxml.NewXpFromFile("testdata/scope.xml")
+	response.QueryDashP(nil, `/saml:Assertion/saml:AttributeStatement[1]/saml:Attribute[@Name='eduPersonPrincipalName']/saml:AttributeValue`, "mekhan@student.aau.dk@aau.dk", nil)
 
-    destinationAttributes := response.Query(nil, `/samlp:Response/saml:Assertion/saml:AttributeStatement`)[0]
-    eppn, securitydomain, err := checkScope(response, idp_md, destinationAttributes, "saml:Attribute[@Name='eduPersonPrincipalName']/saml:AttributeValue")
-    fmt.Println(eppn, securitydomain, err)
-    // Output:
-    // mekhan@student.aau.dk@aau.dk student.aau.dk@aau.dk <nil>
+	destinationAttributes := response.Query(nil, `/samlp:Response/saml:Assertion/saml:AttributeStatement`)[0]
+	eppn, securitydomain, err := checkScope(response, idp_md, destinationAttributes, "saml:Attribute[@Name='eduPersonPrincipalName']/saml:AttributeValue")
+	fmt.Println(eppn, securitydomain, err)
+	// Output:
+	// mekhan@student.aau.dk@aau.dk student.aau.dk@aau.dk <nil>
 }
 
 func ExampleCheckScopeAAU1() {
-    response := goxml.NewXpFromFile("testdata/sourceresponse_dtu.saml")
-    idp_md := goxml.NewXpFromFile("testdata/scope.xml")
-    response.QueryDashP(nil, `/saml:Assertion/saml:AttributeStatement[1]/saml:Attribute[@Name='eduPersonPrincipalName']/saml:AttributeValue`, "mekhan@aau.dk", nil)
+	response := goxml.NewXpFromFile("testdata/sourceresponse_dtu.saml")
+	idp_md := goxml.NewXpFromFile("testdata/scope.xml")
+	response.QueryDashP(nil, `/saml:Assertion/saml:AttributeStatement[1]/saml:Attribute[@Name='eduPersonPrincipalName']/saml:AttributeValue`, "mekhan@aau.dk", nil)
 
-    destinationAttributes := response.Query(nil, `/samlp:Response/saml:Assertion/saml:AttributeStatement`)[0]
-    eppn, securitydomain, err := checkScope(response, idp_md, destinationAttributes, "saml:Attribute[@Name='eduPersonPrincipalName']/saml:AttributeValue")
-    fmt.Println(eppn, securitydomain, err)
-    // Output:
-    // mekhan@aau.dk aau.dk <nil>
+	destinationAttributes := response.Query(nil, `/samlp:Response/saml:Assertion/saml:AttributeStatement`)[0]
+	eppn, securitydomain, err := checkScope(response, idp_md, destinationAttributes, "saml:Attribute[@Name='eduPersonPrincipalName']/saml:AttributeValue")
+	fmt.Println(eppn, securitydomain, err)
+	// Output:
+	// mekhan@aau.dk aau.dk <nil>
 }
 
 func ExampleInvalidScope() {
-    response := goxml.NewXpFromFile("testdata/sourceresponse_dtu.saml")
-    idp_md := goxml.NewXpFromFile("testdata/scope.xml")
-    response.QueryDashP(nil, `/saml:Assertion/saml:AttributeStatement[1]/saml:Attribute[@Name='eduPersonPrincipalName']/saml:AttributeValue`, "mekhan@nu.edu.pk", nil)
+	response := goxml.NewXpFromFile("testdata/sourceresponse_dtu.saml")
+	idp_md := goxml.NewXpFromFile("testdata/scope.xml")
+	response.QueryDashP(nil, `/saml:Assertion/saml:AttributeStatement[1]/saml:Attribute[@Name='eduPersonPrincipalName']/saml:AttributeValue`, "mekhan@nu.edu.pk", nil)
 
-    destinationAttributes := response.Query(nil, `/samlp:Response/saml:Assertion/saml:AttributeStatement`)[0]
-    eppn, securitydomain, err := checkScope(response, idp_md, destinationAttributes, "saml:Attribute[@Name='eduPersonPrincipalName']/saml:AttributeValue")
-    fmt.Println(eppn, securitydomain, err)
-    // Output:
-    // mekhan@nu.edu.pk nu.edu.pk security domain 'nu.edu.pk' does not match any scopes
+	destinationAttributes := response.Query(nil, `/samlp:Response/saml:Assertion/saml:AttributeStatement`)[0]
+	eppn, securitydomain, err := checkScope(response, idp_md, destinationAttributes, "saml:Attribute[@Name='eduPersonPrincipalName']/saml:AttributeValue")
+	fmt.Println(eppn, securitydomain, err)
+	// Output:
+	// mekhan@nu.edu.pk nu.edu.pk security domain 'nu.edu.pk' does not match any scopes
 }
 
 func ExampleNoScope() {
-    response := goxml.NewXpFromFile("testdata/sourceresponse_dtu.saml")
-    idp_md := goxml.NewXpFromFile("testdata/scope.xml")
-    response.QueryDashP(nil, `/saml:Assertion/saml:AttributeStatement[1]/saml:Attribute[@Name='eduPersonPrincipalName']/saml:AttributeValue`, " ", nil)
-    destinationAttributes := response.Query(nil, `/samlp:Response/saml:Assertion/saml:AttributeStatement`)[0]
+	response := goxml.NewXpFromFile("testdata/sourceresponse_dtu.saml")
+	idp_md := goxml.NewXpFromFile("testdata/scope.xml")
+	response.QueryDashP(nil, `/saml:Assertion/saml:AttributeStatement[1]/saml:Attribute[@Name='eduPersonPrincipalName']/saml:AttributeValue`, " ", nil)
+	destinationAttributes := response.Query(nil, `/samlp:Response/saml:Assertion/saml:AttributeStatement`)[0]
 
-    eppn, securitydomain, err := checkScope(response, idp_md, destinationAttributes, "saml:Attribute[@Name='eduPersonPrincipalName']/saml:AttributeValue")
-    fmt.Println(eppn, securitydomain, err)
-    // Output:
-    // not a scoped value:
+	eppn, securitydomain, err := checkScope(response, idp_md, destinationAttributes, "saml:Attribute[@Name='eduPersonPrincipalName']/saml:AttributeValue")
+	fmt.Println(eppn, securitydomain, err)
+	// Output:
+	// not a scoped value:
 }
