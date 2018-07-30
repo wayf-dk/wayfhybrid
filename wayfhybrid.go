f56a84db647b696e8cecdd41a272a4c7e1a4d38a
Mads Freek <freek@wayf.dk>
2018-07-30 14:07:19 +0200

    Updated to use goxml.RmElement

--- wayfhybrid.go
+++ wayfhybrid.go
@@ -932,8 +932,7 @@ func WayfACSServiceHandler(idpMd, hubMd, spMd, request, response *goxml.Xp, birk
 		}
 	}
 
-	parent, _ := sourceAttributes.ParentNode()
-	goxml.RemoveChild(parent, sourceAttributes)
+	goxml.RmElement(sourceAttributes)
 
 	// check that the security domain of eppn is one of the domains in the shib:scope list
 	// we just check that everything after the (leftmost|rightmost) @ is in the scope list and save the value for later
