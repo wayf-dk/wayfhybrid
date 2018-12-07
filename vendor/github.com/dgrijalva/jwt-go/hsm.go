2ae7a5a6926b957fc85fef2ca6f873f828589632
Mads Freek <freek@wayf.dk>
2018-12-08 00:00:55 +0100

    Added hsm.go to jwt package to support signing with private key in HSM

---
+++ vendor/github.com/dgrijalva/jwt-go/hsm.go
@@ -0,0 +1,37 @@
+package jwt
+
+import (
+	"crypto"
+	"github.com/wayf-dk/goxml"
+)
+
+// Implements the RSA family of signing methods signing methods
+// Expects *rsa.PrivateKey for signing and *rsa.PublicKey for validation
+type SigningMethodHSM struct {
+	Name, Algo string
+}
+
+// Specific instances for RS256 and company
+var (
+	SigningMethodHSM256 *SigningMethodHSM
+)
+
+func init() {
+	SigningMethodHSM256 = &SigningMethodHSM{"HSM256", "SHA256"}
+	RegisterSigningMethod(SigningMethodHSM256.Alg(), func() SigningMethod {
+		return SigningMethodHSM256
+	})
+}
+
+func (m *SigningMethodHSM) Alg() string {
+	return m.Name
+}
+
+func (m *SigningMethodHSM) Sign(signingString string, key interface{}) (string, error) {
+	digest := goxml.Hash(goxml.Algos[m.Algo].Algo, signingString)
+	if sigBytes, err := goxml.Sign(digest, privatekey.(string), "", m.Algo); err == nil {
+		return EncodeSegment(sigBytes), nil
+	} else {
+		return "", err
+	}
+}
