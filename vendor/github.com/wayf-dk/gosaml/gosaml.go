a31d88f4fc97ca67d049f68a991a848531a86693
Mads Freek <freek@wayf.dk>
2019-03-12 16:27:20 +0100

    Exported FindInMetadataSets

--- vendor/github.com/wayf-dk/gosaml/gosaml.go
+++ vendor/github.com/wayf-dk/gosaml/gosaml.go
@@ -348,7 +348,7 @@ func inArray(item string, array []string) bool {
 	return false
 }
 
-func findInMetadataSets(metadataSets MdSets, key string) (md *goxml.Xp, index int, err error) {
+func FindInMetadataSets(metadataSets MdSets, key string) (md *goxml.Xp, index int, err error) {
     for index, _ = range metadataSets {
         md, err = metadataSets[index].MDQ(key)
         if err == nil { // if we don't get md not found the last error is as good as the first
@@ -439,7 +439,7 @@ func DecodeSAMLMsg(r *http.Request, issuerMdSets, destinationMdSets MdSets, role
 		return
 	}
 
-    issuerMd, issuerIndex, err = findInMetadataSets(issuerMdSets, issuer)
+    issuerMd, issuerIndex, err = FindInMetadataSets(issuerMdSets, issuer)
 	if err != nil {
 	    return
 	}
@@ -455,7 +455,7 @@ func DecodeSAMLMsg(r *http.Request, issuerMdSets, destinationMdSets MdSets, role
 		return
 	}
 
-    destinationMd, destinationIndex, err = findInMetadataSets(destinationMdSets, location)
+    destinationMd, destinationIndex, err = FindInMetadataSets(destinationMdSets, location)
 	if err != nil {
 		return
 	}
@@ -1128,12 +1128,12 @@ func wsfedRequest2samlRequest(r *http.Request, issuerMdSets, destinationMdSets M
 		issuer := r.Form.Get("wtrealm")
 		location := "https://" + r.Host + r.URL.Path
 
-        destinationMd, _, err := findInMetadataSets(destinationMdSets, location)
+        destinationMd, _, err := FindInMetadataSets(destinationMdSets, location)
         if err != nil {
             return
         }
 
-        issuerMd, _, err := findInMetadataSets(issuerMdSets, issuer)
+        issuerMd, _, err := FindInMetadataSets(issuerMdSets, issuer)
 
         if err != nil {
             return
