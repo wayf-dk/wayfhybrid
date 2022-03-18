e28f092d8524fc157ef7823139d56aa1378cf5f7
Mads Freek <freek@wayf.dk>
2022-03-18 10:05:30 +0100

    Replaced beta nemlog-in3 with the integration env nemlog-in3

--- wayfattributehandler.go
+++ wayfattributehandler.go
@@ -39,8 +39,8 @@ var (
 		{c14n: "Issuer", op: "xp:msg:./saml:Issuer"},
 
 		// nemlogin computed
-		{c14n: "nemlogin", op: "eq:Issuer:https://saml.test-devtest4-nemlog-in.dk"},
-		{c14n: "nemlogin", op: "eq:Issuer:https://saml.nemlog-in.dk"},
+		{c14n: "nemlogin", op: "eq:Issuer:https://saml-nemlog-in.dk"},
+		{c14n: "nemlogin", op: "eq:Issuer:https://saml.test-nemlog-in.dk/"},
 		{c14n: "eduPersonPrimaryAffiliation", name: "eduPersonPrimaryAffiliation", op: "nemlogin:val:member"},
 		{c14n: "organizationName", op: "nemlogin:val:NemLog-in"},
 		{c14n: "schacPersonalUniqueID", name: "schacPersonalUniqueID", op: "nemlogin:prefix:urn:mace:terena.org:schac:personalUniqueID:dk:CPR:"},
