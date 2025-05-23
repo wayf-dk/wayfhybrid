e5eea6916d96a574cf2da5e1d78cd5d7629fe461
Mads Freek <freek@wayf.dk>
2025-05-23 11:44:30 +0200

    Removed content-type mapping from fileserver - openid-configuration i now handled dynamically

--- wayfhybrid.go
+++ wayfhybrid.go
@@ -227,7 +227,7 @@ func Main() {
 	httpMux.Handle(config.Dstiming, appHandler(godiscoveryservice.DSTiming))
 
 	fs := http.FileServer(http.FS(config.PublicFiles))
-	suffixes := map[string]string{".jwk": "application/jwk+json", ".well-known/openid-configuration": "application/json"}
+	suffixes := map[string]string{".jwk": "application/jwk+json"}
 	f := func(w http.ResponseWriter, r *http.Request) (err error) {
 		for suffix, mimetype := range suffixes {
 			if strings.HasSuffix(r.RequestURI, suffix) {
